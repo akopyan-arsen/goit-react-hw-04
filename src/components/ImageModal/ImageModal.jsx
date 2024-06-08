ee3653f108010adbfe4dd7ad887c9c16d5d75dcd
Arsen Akopyan <tigrikpictures@gmail.com>
2024-06-08 17:58:05 +0300

    fix modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -10,7 +10,7 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-    width: "700px",
+    width: "50%",
     padding: "0",
     borderRadius: "20px",
   },
